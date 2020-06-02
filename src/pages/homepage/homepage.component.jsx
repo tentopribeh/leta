8ca02adf756f3e0078188db2320bf8cbd5838c74
agatatalita <agatamonikaandrzejewska@gmail.com>
2020-06-03 01:28:30 +0200

    clean up a little, add svg loader

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import wtf from 'wtf_wikipedia';
 
 import exampleText from '../../data/default-text';
+import { ReactComponent as Logo} from '../../data/loader.svg';
 
 import './homepage.styles.scss';
 
@@ -11,6 +12,8 @@ const HomePage = () => {
     const [mostFrequentWords, setMostFrequentWords] = React.useState([]);
     const [wordsLimit, setWordsLimit] = React.useState(20);
     const [wordsOffset, setWordsOffset] = React.useState(0);
+    const [loadingStarted, setLoadingStarted] = React.useState(false)
+    console.clear();
     console.log(`cze­kam cze­kam wy­trwa­le
     tak lek­ko do­ty­ka­ją mnie dni
     moja tę­sk­no­ta jest tę­sk­no­tą pla­net
@@ -19,8 +22,10 @@ const HomePage = () => {
     na da­chach blask księżyca lśni
     wą­skie wie­że ko­ścio­łów na­kłu­wa­ją nie­bo
     i dni tak lek­ko bie­gną nie wia­do­mo gdzie `)
+    console.log('%c ', 'font-size:1px; padding: 100px 150px; background:url(https://66.media.tumblr.com/36584c3bb4ad641e9060ced5756c85d2/tumblr_pi2opgP0Tw1u8pg83o1_640.png) no-repeat; background-size: contain;');
 
     const WORDS_LIMIT_MAX = 100;
+    const WORDS_OFFSET_MAX = 1000000;
 
     const handleChange = async event => {
         const { value } = event.target;
@@ -29,6 +34,8 @@ const HomePage = () => {
 
     const handleSubmit = async event => {
         event.preventDefault();
+        await setLoadingStarted(true);
+        await setMostFrequentWords([]);
         await chooseWords(text);
     }
 
@@ -45,10 +52,11 @@ const HomePage = () => {
     const handlePagination = async event => {
         let { name, value } = event.target;
         if (value < 0) return;
-        if(name === 'wordOffset') {
-            value = Math.min(value, WORDS_LIMIT_MAX);
+        if(name === 'wordsOffset') {
+            value = Math.min(value, WORDS_OFFSET_MAX);
             setWordsOffset(value)
         } else {
+            value = Math.min(value, WORDS_LIMIT_MAX);
             setWordsLimit(value) 
         }       
     }
@@ -64,8 +72,6 @@ const HomePage = () => {
         // count words
         const wordsDictionary = {};
 
-
-
         for(const word of wordsList) {
             if(word in wordsDictionary){
                 wordsDictionary[word]++;
@@ -73,11 +79,6 @@ const HomePage = () => {
                 wordsDictionary[word] = 1;
             }
         }
-
-        // choose n biggest words
-
-        // Create items array
-        // let str = `# {{Příznaky|cs|zast.|nář.}} [[strýc]] [[z]] [[matčin]]y [[strana|strany]]&lt;ref name=&quot;ssjc6&quot;&gt;Československá akademie věd. Ústav pro jazyk český. ''Slovník spisovného jazyka českého.'' Díl VI. Š-U. Praha: Academia, 1989. Heslo „ujec“, s. 306.&lt;/ref&gt; # {{Příznaky|cs|řidč.}} [[manžel]] [[matčin]]y [[sestra|sestry]]&lt;ref name=&quot;priruc&quot;&gt;Ústav pro jazyk český Československé akademie věd. ''Příruční slovník jazyka českého.'' Díl VI. T-Vůzek. Praha: Státní nakladatelství, 1951-1953. Heslo „ujec“, s. 438, 439.&lt;/ref&gt; # {{Příznaky|cs|nář.}} [[venkovan]], [[soused]]&lt;ref name=&quot;ssjc6&quot;/&gt; # {{Příznaky|cs|nář.}} [[výr]]&lt;ref name=&quot;ssjc6&quot;/&gt; # {{Příznaky|cs|nář.}} [[druh]] [[tlačenka|tlačenky]]&lt;ref name=&quot;priruc&quot;/&gt; # {{Příznaky|cs|nář.}} [[poduška]], [[polštář]]&lt;ref name=&quot;priruc&quot;/&gt; `
 
         let items = await Promise.all(Object.keys(wordsDictionary).map(async function(key) {
 
@@ -89,7 +90,6 @@ const HomePage = () => {
             return second[1] - first[1];
         });
         items = items.slice(wordsOffset, wordsLimit + wordsOffset)
-        console.log(items)
 
         items = await Promise.all(items.map(async function(item) {
             const fetchedText = await wtf.fetch(`https://cs.wiktionary.org/wiki/${item[0]}`);
@@ -100,7 +100,7 @@ const HomePage = () => {
             let meaning = fetchedText.sections('čeština') ? fetchedText.sections('čeština').sections('význam') : fetchedText.sections('význam')
             meaning = meaning || 'brak definicji';
             let meaningText = await wtf(meaning.data.wiki).text();
-            while(meaningText.indexOf('*') != -1) {
+            while(meaningText.indexOf('*') !== -1) {
                 meaningText = meaningText.replace('*', '\n');
             }
             return [item[0], item[1], meaningText ]// await getWordData(key)]
@@ -117,19 +117,25 @@ const HomePage = () => {
     return(
         <div className='home-page'>
             <div className='text-input-box'>
-                <div>Put text to generate words dictionary.</div>
+                <div className='title'>Book to dictionary</div>
+                <div className='explanation'>Put text to generate words dictionary.</div>
                 <form onSubmit={handleSubmit}>
                     <button onClick={handleClean}>Clean</button>
                     <button onClick={handleRandom}>Put random text</button>
                     <textarea className='text-input' value = {text} onChange = {handleChange}></textarea>
-                    How many words would you like to see? (max 100)
-                    <input value={wordsLimit} name={wordsLimit} label="Words limit" type="number" min="0" onChange={handlePagination}></input>
-                    Set offset
-                    <input value={wordsOffset} name={wordsOffset} label="Words limit" type="number" onChange={handlePagination} min="0" max="100"></input>
+                    <br/>
+                    How many words would you like to see? (max 100): 
+                    <input className="input-number" value={wordsLimit} name="wordsLimit" label="Words limit" type="number" min="0"  max="100" onChange={handlePagination}></input>
+                    <br/>
+                    Set offset: 
+                    <input className="input-number" value={wordsOffset} name="wordsOffset" label="Words offset" type="number" onChange={handlePagination} min="0" max="1000000"></input>
+                    <br/>
                     <button>Generate</button>
                 </form>
             </div>
             <div className='words-list-box'>
+                {(mostFrequentWords.length === 0 && loadingStarted) &&  <Logo className="loader" /> }
+                {(mostFrequentWords.length === 0 && !loadingStarted) &&  "Click 'Generate' to create a dictionary" }
                 <div className='words-list'>
                     {mostFrequentWords.map(word => <div className='word-box' key={word[0]}><div className='word'>{word[0]}</div><div className='word-meaning'>{word[2]}</div></div>)}
                 </div>
