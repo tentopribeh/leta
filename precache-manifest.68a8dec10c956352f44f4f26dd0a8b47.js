63f88e6090d73c4b5a066508514aafa73017fb21
agatatalita <agatamonikaandrzejewska@gmail.com>
2020-06-02 10:08:25 +0200

    Updates

---
+++ precache-manifest.68a8dec10c956352f44f4f26dd0a8b47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39aaa6acf2d1ef34c2b18b89f1f23932",
+    "url": "/leta/index.html"
+  },
+  {
+    "revision": "0b8f8ce2a79df8830c35",
+    "url": "/leta/static/css/main.6a9d1ba3.chunk.css"
+  },
+  {
+    "revision": "b7da7ca33ada40a1eb61",
+    "url": "/leta/static/js/2.be50b970.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/leta/static/js/2.be50b970.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b8f8ce2a79df8830c35",
+    "url": "/leta/static/js/main.951f0ccf.chunk.js"
+  },
+  {
+    "revision": "03b10ff2517fa7ab8d81",
+    "url": "/leta/static/js/runtime-main.4ce6d5d9.js"
+  }
+]);
